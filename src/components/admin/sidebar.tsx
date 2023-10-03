4bf190bea907479db5817b74dd93e53d1f71516b
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-10-03 11:40:46 +0700

    sidebar dropdown

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -11,20 +11,16 @@ import {
 
 export default function AdminSidebar() {
     return (<>
-        <div className="flex flex-col flex-grow gap-2 p-4 bg-slate-100">
+        <div className="flex flex-col flex-grow h-screen gap-2 p-4 bg-slate-100">
             <Link href={'/'} className="px-4 text-lg font-bold">LMS</Link>
             <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start' })}>Home</Link>
             <Accordion type="multiple">
-                <AccordionItem value="item-1">
-                    <AccordionTrigger className={buttonVariants({ variant: "ghost", class: '!justify-between !no-underline' })}>Is it accessible?</AccordionTrigger>
-                    <AccordionContent className="flex flex-col">
-                        <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start' })}>Home</Link>
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                    <AccordionTrigger className={buttonVariants({ variant: "ghost", class: '!justify-between !no-underline' })}>Is it accessible?</AccordionTrigger>
-                    <AccordionContent className="flex flex-col">
-                        <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start' })}>Home</Link>
+                <AccordionItem value="item-1" className="!border-none">
+                    <AccordionTrigger className={buttonVariants({ variant: "ghost", class: '!justify-between !no-underline' })}>Manage</AccordionTrigger>
+                    <AccordionContent className="flex flex-col p-2">
+                        <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start w-full' })}>User</Link>
+                        <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start w-full' })}>Student</Link>
+                        <Link href={'/'} className={buttonVariants({ variant: 'ghost', class: '!justify-start w-full' })}>Lecturer</Link>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
