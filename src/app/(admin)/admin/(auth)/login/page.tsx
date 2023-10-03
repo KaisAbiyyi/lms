657d26007d89fa8722086ada0dd5ce53fc340c8f
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-10-03 19:22:02 +0700

    login page

---
+++ src/app/(admin)/admin/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminLogin() {
+    return (<>
+        <h1>admin login</h1>
+    </>)
+}
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,13 +1,19 @@
+"use client"
+
 import AdminSidebar from "@/components/admin/sidebar"
+import { usePathname } from "next/navigation"
 
 export default function AdminLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const path = usePathname()
     return (
         <div className="flex gap-4">
-            <AdminSidebar />
+            {!path.match('/admin/login') &&
+                <AdminSidebar />
+            }
             <div className="w-5/6">
                 {children}
             </div>
