4c1a848c1746639937e3e96c5593b8466ea7c00f
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-10-09 21:04:15 +0700

    login form

--- src/app/(admin)/admin/(auth)/login/page.tsx
+++ src/app/(admin)/admin/(auth)/login/page.tsx
@@ -1,5 +1,30 @@
+import LoginForm from "@/components/admin/auth/LoginForm";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
 export default function AdminLogin() {
     return (<>
-        <h1>admin login</h1>
+        <h1>
+            <div className="w-screen h-screen flex justify-center items-center">
+                <Card className="m-auto w-3/6 flex overflow-hidden">
+                    <div className="w-1/2 bg-blue-500">
+                        <CardHeader>
+                            <CardTitle className="text-slate-100">LMS</CardTitle>
+                            <Image className="mx-auto" src={"/assets/images/study.png"} width={300} height={300} alt="study ilustration" />
+                            <CardDescription className="text-blue-200">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt numquam quae sapiente aut nam sed blanditiis repellendus ullam quia aliquid!</CardDescription>
+                        </CardHeader>
+                    </div>
+                    <div className="w-1/2 p-4">
+                        <CardHeader>
+                            <CardTitle>Welcome Back!</CardTitle>
+                            <CardDescription>User your credentials to access your account.</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <LoginForm />
+                        </CardContent>
+                    </div>
+                </Card>
+            </div>
+        </h1>
     </>)
 }
---
+++ src/components/admin/auth/LoginForm.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+    email: z.string().min(1, {
+        message: "Email cannot null.",
+    }).email(),
+    password: z.string().min(1, {
+        message: "Password cannot null.",
+    }),
+})
+
+export default function LoginForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: ''
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+        console.log(formSchema)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="Enter your email here..." {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Enter your password here..." {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" variant={"default"} className="w-full">Submit</Button>
+            </form>
+        </Form>
+    )
+}
