3ae1e61af8eb11594966a43d729b1e2a7011a6f2
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-11-07 22:22:16 +0700

    lecturer page

--- src/app/(admin)/admin/(management)/lecturer/page.tsx
+++ src/app/(admin)/admin/(management)/lecturer/page.tsx
@@ -1,5 +1,21 @@
 "use client"
 
-export default function LecturerManagementPage(){
-    return "this is lecurer"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function LecturerManagementPage() {
+    return (
+        <>
+            <div className="flex flex-col gap-8">
+                <h1 className="text-2xl font-semibold text-slate-700">Lecturer</h1>
+                <div className="flex gap-4">
+                    <Card>
+                        <CardHeader>
+                            <CardDescription>Total</CardDescription>
+                            <CardTitle>12</CardTitle>
+                        </CardHeader>
+                    </Card>
+                </div>
+            </div>
+        </>
+    )
 }
